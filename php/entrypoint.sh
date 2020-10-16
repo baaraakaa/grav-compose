1cd78152a4027d4ecef4fd4211497139fd3c32bf
Snail Mail <>
2020-10-15 18:13:17 -0700

    solve file ownership issue

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-chown -R www-data /code
+# chown -R www-data /code
 chgrp -R www-data /code
-
+find /code -type f -exec chmod 664 {} \;
+find /code -type f -exec chmod 775 {} \;
+find /code -type d -exec chmod 775 {} \;
+find /code -type d -exec chmod +s {} \;
 
 # docker-php-entrypoint
 set -e
